3bf1e45007fdc838ed89ea57c03058f196a7b384
Caitano <josias.m.caitano@accenture.com>
2023-07-01 13:01:50 -0300

    adding BaseResourceModel

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -1,6 +1,7 @@
+import { BaseResourceModel } from "src/app/shared/models/base-resource.model";
 import { Category } from "../../categories/shared/category.model";
 
-export class Entry {
+export class Entry extends BaseResourceModel {
 
   constructor(
     public id?: number,
@@ -12,7 +13,9 @@ export class Entry {
     public paid?: boolean,
     public categoryId?: number,
     public category?: Category,
-  ) {}
+  ) {
+    super();
+  }
 
   static types = {
     expense: 'Despesa',
