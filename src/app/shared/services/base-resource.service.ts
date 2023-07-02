de0bc42b29474fbd481b051223e430c667b9a0ca
Caitano <josias.m.caitano@accenture.com>
2023-07-02 09:13:00 -0300

    improving BaseResourceService && refactoring CategoryService

---
+++ src/app/shared/services/base-resource.service.ts
@@ -0,0 +1,75 @@
+import { Observable, throwError } from "rxjs";
+import { BaseResourceModel } from "../models/base-resource.model";
+import { catchError, map } from "rxjs/Operators";
+import { HttpClient } from "@angular/common/http";
+import { Inject } from "@angular/compiler/src/core";
+import { Injector } from "@angular/core";
+
+export abstract class BaseResourceService<T extends BaseResourceModel> {
+
+  protected http: HttpClient;
+
+  constructor(protected apiPath: string, protected injector: Injector) {
+    this.http = injector.get(HttpClient);
+    console.log(apiPath, 'ibg url')
+  }
+
+  getAll(): Observable<T[]> {
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDatToResources)
+    )
+  }
+
+  getById(id: number): Observable<T> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToResource)
+    );
+  }
+
+  create(resource: T): Observable<T> {
+    return this.http.post(this.apiPath, resource).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToResource)
+    )
+  }
+
+  update(resource: T): Observable<T> {
+    const url = `${this.apiPath}/${resource.id}`;
+
+    return this.http.put(url, resource).pipe(
+      catchError(this.handleError),
+      map(() => resource)
+    )
+  }
+
+  delete(id: number): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    )
+  }
+
+   // procted methods
+
+   protected jsonDatToResources(jsonData: any[]): T[] {
+    const resources: T[] = [];
+    jsonData.forEach(element => resources.push(element as T));
+    return resources;
+  }
+
+  protected jsonDataToResource(jsonData: any): T {
+    return jsonData as T;
+  }
+
+  protected handleError(error: any) {
+    console.log("ERRO NA REQUISIÇÃO =>, ", error);
+    return throwError(error);
+  }
+
+}
