d4150addfef7767e5f8beda43c8a0f0d5c0e4a76
Caitano <josias.m.caitano@accenture.com>
2023-07-03 17:12:45 -0300

    adding RouterModule to SharedModule && improving page components with BreadCrumbComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [BreadCrumbComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    RouterModule
   ],
   exports: [
-    // shared
+    // shared modules
     CommonModule,
     ReactiveFormsModule,
+    RouterModule,
 
     // shared components
     BreadCrumbComponent
