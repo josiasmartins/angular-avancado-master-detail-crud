e750f142435656c226708d6a1333ab0a54d82a56
Caitano <josias.m.caitano@accenture.com>
2023-07-03 20:08:11 -0300

    improving PageHeaderComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { RouterModule } from '@angular/router';
-import { PageHeaderComponent } from './components/page-header/page-header.component';
+
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { ServerErrorMessagesComponent } from './components/server-error-messages/server-error-messages.component';
 
 @NgModule({
